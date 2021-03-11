20aa3ba01a2f4734a5ef63fc7f7a334f633fa1a8
mnb27 <2018csb1069@iitrpr.ac.in>
2021-03-11 13:23:00 +0530

    added tag buttons to record doctor data

--- app/src/main/java/com/example/maternalmortality/DataForDoctor.kt
+++ app/src/main/java/com/example/maternalmortality/DataForDoctor.kt
@@ -1,18 +1,19 @@
 package com.example.maternalmortality
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.maternalmortality.models.DoctorPatientDetails
 import com.example.maternalmortality.models.PatientDetails
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
+
 
 class DataForDoctor : AppCompatActivity() {
 
@@ -48,20 +49,143 @@ class DataForDoctor : AppCompatActivity() {
                     }
                     else{
                         val fever: TextInputLayout = findViewById(R.id.one)
-                        val headache: TextInputLayout = findViewById(R.id.two)
-                        val blurvision: TextInputLayout = findViewById(R.id.three)
-                        val swelling: TextInputLayout = findViewById(R.id.four)
-                        val puffedface: TextInputLayout = findViewById(R.id.five)
-                        val palpitations: TextInputLayout = findViewById(R.id.six)
-                        val fatigability: TextInputLayout = findViewById(R.id.seven)
-                        val breathlessness: TextInputLayout = findViewById(R.id.eight)
-                        val abdomenpain: TextInputLayout = findViewById(R.id.nine)
-                        val vaginalbleeding: TextInputLayout = findViewById(R.id.ten)
-                        val waterdischarge: TextInputLayout = findViewById(R.id.eleven)
-                        val fatalmovements: TextInputLayout = findViewById(R.id.twelve)
+                        val headache: Button = findViewById(R.id.two)
+                        val blurvision: Button = findViewById(R.id.three)
+                        val swelling: Button = findViewById(R.id.four)
+                        val puffedface: Button = findViewById(R.id.five)
+                        val palpitations: Button = findViewById(R.id.six)
+                        val fatigability: Button = findViewById(R.id.seven)
+                        val breathlessness: Button = findViewById(R.id.eight)
+                        val abdomenpain: Button = findViewById(R.id.nine)
+                        val vaginalbleeding: Button = findViewById(R.id.ten)
+                        val waterdischarge: Button = findViewById(R.id.eleven)
+                        val fatalmovements: Button = findViewById(R.id.twelve)
 
                         val saveButton: Button = findViewById(R.id.saveButton)
 
+                        // color change on button click
+                        var clickHeadache = true
+                        headache.setOnClickListener(View.OnClickListener {
+                            clickHeadache = if (clickHeadache) {
+//                                headache.setBackgroundColor(Color.parseColor("#228B22"))
+                                headache.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                headache.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickblurvision = true
+                        blurvision.setOnClickListener(View.OnClickListener {
+                            clickblurvision = if (clickblurvision) {
+                                blurvision.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                blurvision.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickswelling = true
+                        swelling.setOnClickListener(View.OnClickListener {
+                            clickswelling = if (clickswelling) {
+                                swelling.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                swelling.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickpuffedface = true
+                        puffedface.setOnClickListener(View.OnClickListener {
+                            clickpuffedface = if (clickpuffedface) {
+                                puffedface.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                puffedface.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickpalpitations = true
+                        palpitations.setOnClickListener(View.OnClickListener {
+                            clickpalpitations = if (clickpalpitations) {
+                                palpitations.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                palpitations.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickfatigability = true
+                        fatigability.setOnClickListener(View.OnClickListener {
+                            clickfatigability = if (clickfatigability) {
+                                fatigability.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                fatigability.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickbreathlessness = true
+                        breathlessness.setOnClickListener(View.OnClickListener {
+                            clickbreathlessness = if (clickbreathlessness) {
+                                breathlessness.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                breathlessness.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickabdomenpain = true
+                        abdomenpain.setOnClickListener(View.OnClickListener {
+                            clickabdomenpain = if (clickabdomenpain) {
+                                abdomenpain.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                abdomenpain.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickvaginalbleeding = true
+                        vaginalbleeding.setOnClickListener(View.OnClickListener {
+                            clickvaginalbleeding = if (clickvaginalbleeding) {
+                                vaginalbleeding.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                vaginalbleeding.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickwaterdischarge = true
+                        waterdischarge.setOnClickListener(View.OnClickListener {
+                            clickwaterdischarge = if (clickwaterdischarge) {
+                                waterdischarge.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                waterdischarge.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+
+                        var clickfatalmovements = true
+                        fatalmovements.setOnClickListener(View.OnClickListener {
+                            clickfatalmovements = if (clickfatalmovements) {
+                                fatalmovements.setBackgroundResource(R.drawable.rounded_button_green)
+                                false
+                            } else {
+                                fatalmovements.setBackgroundResource(R.drawable.rounded_button_blue)
+                                true
+                            }
+                        })
+                        ///////////////////////////////////////////////END
 
                         var anm_email = previousDetails?.anm_supervisior_email.toString()
                         var asha_email = previousDetails?.asha_supervisor_email.toString()
@@ -73,18 +197,39 @@ class DataForDoctor : AppCompatActivity() {
 
                         saveButton.setOnClickListener {
                             var feverText = fever.editText?.text.toString()
-                            var headacheText = headache.editText?.text.toString()
-                            var blurvisonText = blurvision.editText?.text.toString()
-                            var swellingText = swelling.editText?.text.toString()
-                            var puffedfaceText = puffedface.editText?.text.toString()
-                            var palpitationsText = palpitations.editText?.text.toString()
-                            var fatigabilityText = fatigability.editText?.text.toString()
-                            var breathlessnessText = breathlessness.editText?.text.toString()
-                            var abdomenpainText = abdomenpain.editText?.text.toString()
-                            var vaginalbleedingText = vaginalbleeding.editText?.text.toString()
-                            var waterdischargeText = waterdischarge.editText?.text.toString()
-                            var fatalmovementsText = fatalmovements.editText?.text.toString()
 
+                            var headacheText = "NO"
+                            if(clickHeadache==false) headacheText="YES"
+
+                            var blurvisonText = "NO"
+                            if(clickblurvision==false) blurvisonText="YES"
+
+                            var swellingText = "NO"
+                            if(clickswelling==false) swellingText="YES"
+
+                            var puffedfaceText = "NO"
+                            if(clickpuffedface==false) puffedfaceText="YES"
+
+                            var palpitationsText = "NO"
+                            if(clickpalpitations==false) palpitationsText="YES"
+
+                            var fatigabilityText = "NO"
+                            if(clickfatigability==false) fatigabilityText="YES"
+
+                            var breathlessnessText = "NO"
+                            if(clickbreathlessness==false) breathlessnessText="YES"
+
+                            var abdomenpainText = "NO"
+                            if(clickabdomenpain==false) abdomenpainText="YES"
+
+                            var vaginalbleedingText = "NO"
+                            if(clickvaginalbleeding==false) vaginalbleedingText="YES"
+
+                            var waterdischargeText = "NO"
+                            if(clickwaterdischarge==false) waterdischargeText="YES"
+
+                            var fatalmovementsText = "NO"
+                            if(clickfatalmovements==false) fatalmovementsText="YES"
 
                             val patientDetails = DoctorPatientDetails(
                                 feverText,
